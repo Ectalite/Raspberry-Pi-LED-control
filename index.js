c3bbc3bb41ad33e626356be37f333189502456a5
Ectalite <Xaxa.hueber@gmail.com>
2018-06-28 01:50:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 console.log(path.join(__dirname, 'public')); 
 
 app.get('/', function(req, res){ 
- 	res.render('index',{status:"Press Button To change Status of Led !!"});
+ 	res.render('index',{status:"Press Button To change Status of Led !!"},{statusled1:"Press Button To change Status of Led !!"});
 });
 
 	<!-- ----- LEDs ON ------ -->
