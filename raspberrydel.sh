ad84f6791e039000ec6fd8504eb9655e7585d832
Ectalite <Xaxa.hueber@gmail.com>
2018-06-28 01:40:52 -0700

    Update raspberrydel.sh

--- raspberrydel.sh
+++ raspberrydel.sh
@@ -1,5 +1,6 @@
 rm -r -d /home/pi/raspberry-pi-led-control/
 git clone https://github.com/Ectalite/Raspberry-Pi-LED-control
+chmod 7777 raspberry-pi-led-control
 cd raspberry-pi-led-control/
 npm install 
 cd Nodetest/
