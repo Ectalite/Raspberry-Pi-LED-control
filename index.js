757cd9ea8c5e6843a4fc101d798cba7e7bd9d40c
Ectalite <Xaxa.hueber@gmail.com>
2018-06-28 02:10:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ var LED2 = new Gpio(17, 'out'); 	<!-- See https://lc.cx/m6hz -->
 var LED3 = new Gpio(27, 'out');
 var LED4 = new Gpio(22, 'out');
 var LED5 = new Gpio(23, 'out');
-
-
+var statusled1 = "Led 1 éteinte";
+var statusled2 = "Led 2 éteinte";
+var statusled3 = "Led 3 éteinte";
+var statusled4 = "Led 4 éteinte";
+var statusled5 = "Led 5 éteinte";
 
 app.set('view engine', 'ejs');
 
@@ -29,44 +32,50 @@ app.post('/led1/on', function(req, res){
 <!-- For LED 2 -->
 app.post('/led2/on', function(req, res){
 	LED2.writeSync(1); //set pin state to 1 (turn LED on)
+	res.render('index',{statusled1:"Led 2 en fonction"});
 });
 <!-- For LED 3 -->
 app.post('/led3/on', function(req, res){
 	LED3.writeSync(1); //set pin state to 1 (turn LED on)
+	res.render('index',{statusled1:"Led 3 en fonction"});
 });
 <!-- For LED 4 -->
 app.post('/led4/on', function(req, res){
 	LED4.writeSync(1); //set pin state to 1 (turn LED on)
+	res.render('index',{statusled1:"Led 4 en fonction"});
 });
 <!-- For LED 5 -->
 app.post('/led5/on', function(req, res){
 	LED5.writeSync(1); //set pin state to 1 (turn LED on)
+	res.render('index',{statusled1:"Led 5 en fonction"});
 });
 
 	<!-- LED off -->
 <!-- For LED 1 -->
 app.post('/led1/off', function(req, res){
 	LED.writeSync(0);
+	res.render('index',{statusled1:"Led 1 éteinte"});
 
 });
 <!-- For LED 2 -->
 app.post('/led2/off', function(req, res){
 	LED2.writeSync(0);
-
+	res.render('index',{statusled1:"Led 2 éteinte"});
 });
 <!-- For LED 3 -->
 app.post('/led3/off', function(req, res){
 	LED3.writeSync(0);
-
+	res.render('index',{statusled1:"Led 3 éteinte"});
 });
 <!-- For LED 4 -->
 app.post('/led4/off', function(req, res){
 	LED4.writeSync(0);
+	res.render('index',{statusled1:"Led 4 éteinte"});
 });
 <!-- For LED 5 -->
 app.post('/led5/off', function(req, res){
 	LED5.writeSync(0);
-
+	res.render('index',{statusled1:"Led  5éteinte"});
 });
 
 
